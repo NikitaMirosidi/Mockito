bbf76ea76e1f26695fcd6fd1908cab32d11de5ea
Nikita <dr_mario@ua.fm>
2021-07-05 13:53:00 +0300

    third

--- src/test/java/StoreTest.java
+++ src/test/java/StoreTest.java
@@ -5,7 +5,14 @@ public class StoreTest {
 
     @Test
     public void testGetPrice() {
-        String test = "ABCDABA";
-        Assert.assertEquals(13.25, new Store().getPrice(test),0);
+        String test1 = "ABCDABA";
+        String test2 = "abcdaba";
+        String test3 = "f a,@B' c)/d Ж+a.Rb $a ";
+        String test4 = "";
+
+        Assert.assertEquals(13.25, new Store().getPrice(test1),0);
+        Assert.assertEquals(13.25, new Store().getPrice(test2),0);
+        Assert.assertEquals(13.25, new Store().getPrice(test3),0);
+        Assert.assertEquals(0, new Store().getPrice(test4),0);
     }
 }
